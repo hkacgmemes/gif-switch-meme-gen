8330195fbf54d17f1377a222461b1624baf9de29
相馬 <souma@acgmemes.hk>
2020-04-11 22:21:54 +0800

    是但啦

--- src/js/main.js
+++ src/js/main.js
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
 		html2canvas(drawer, { scrollY: -window.scrollY })
 			.then(canvas => {
 				otherDivs.forEach(div => div.style.display = "");
-				canvas.toBlob(blob => FileSaver.saveAs(blob, "gifbb又迫害咗一隻game.png"));
+				canvas.toBlob(blob => FileSaver.saveAs(blob, "gifbb又迫害咗一隻game.jpg"), .95);
 			});
 	});
 	
