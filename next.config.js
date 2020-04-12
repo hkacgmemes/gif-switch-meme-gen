fb3bdf129e9e28c86f899cf760fa0987056b19bd
相馬 <souma@acgmemes.hk>
2020-04-13 06:10:18 +0800

    是但啦

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 const withImages = require('next-images')
 const withCSS = require('@zeit/next-css')
 const withSass = require('@zeit/next-sass');
 
 const nextConfig = {
+	assetPrefix: isProd ? 'https://hkacgmemes.github.io/gif-switch-meme-gen/' : '',
 	// webpack: function (config) {
 	// 	config.module.rules.push({
 	// 		test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
