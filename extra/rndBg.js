1a1aa320761cac1baeb2ea07407094e21dcd7bc1
相馬 <souma@acgmemes.hk>
2020-04-14 18:11:09 +0800

    是但啦

--- extra/rndBg.js
+++ extra/rndBg.js
@@ -1,20 +1,26 @@
 export default function () {
 	if (typeof document !== 'undefined') {	
 		const backgrounds = [
-			require("./acg_is_not_fk_politics/0.gif"),
-			require("./acg_is_not_fk_politics/1.gif"),
-			require("./acg_is_not_fk_politics/2.gif"),
-			require("./acg_is_not_fk_politics/3.gif"),
-			require("./acg_is_not_fk_politics/4.gif"),
-			require("./acg_is_not_fk_politics/5.gif"),
-			require("./acg_is_not_fk_politics/6.gif"),
-			require("./acg_is_not_fk_politics/7.gif"),
-			require("./acg_is_not_fk_politics/8.gif"),
-			require("./acg_is_not_fk_politics/9.gif"),
+			require("./acg_is_not_fk_politics/00.gif"),
+			require("./acg_is_not_fk_politics/01.gif"),
+			require("./acg_is_not_fk_politics/02.gif"),
+			require("./acg_is_not_fk_politics/03.gif"),
+			require("./acg_is_not_fk_politics/04.gif"),
+			require("./acg_is_not_fk_politics/05.gif"),
+			require("./acg_is_not_fk_politics/06.gif"),
+			require("./acg_is_not_fk_politics/07.gif"),
+			require("./acg_is_not_fk_politics/08.gif"),
+			require("./acg_is_not_fk_politics/09.gif"),
 			require("./acg_is_not_fk_politics/10.gif"),
 			require("./acg_is_not_fk_politics/11.gif"),
 			require("./acg_is_not_fk_politics/12.gif"),
 			require("./acg_is_not_fk_politics/13.gif"),
+			require("./acg_is_not_fk_politics/14.gif"),
+			require("./acg_is_not_fk_politics/15.gif"),
+			require("./acg_is_not_fk_politics/16.gif"),
+			require("./acg_is_not_fk_politics/17.gif"),
+			require("./acg_is_not_fk_politics/18.gif"),
+			require("./acg_is_not_fk_politics/19.gif"),
 		];
 		const randomBgIndex = parseInt(Math.random() * backgrounds.length);
 		document.body.style.background = `url(${backgrounds[randomBgIndex]})`;
